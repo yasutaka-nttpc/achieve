327a083732f809b12184050129a5247391fc4d21
yasutaka-nttpc <yasutaka@nttpc.co.jp>
2017-12-02 23:07:54 +0900

    コメント編集・削除機能追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, only:[:edit, :update, :destroy]
+
   def create
     # Blogをパラメータの値から探し出し,Blogに紐づくcommentsとしてbuildします。
     @comment = current_user.comments.build(comment_params)
@@ -14,9 +16,34 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit
+    @blog = @comment.blog
+  end
+
+  def update
+    if @comment.update(comment_params)
+      redirect_to blog_path(@comment.blog), notice: 'コメントを編集しました。'
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    respond_to do |format|
+      if @comment.destroy
+        format.html{ redirect_to blog_path(@comment.blog), notice: 'コメントを削除しました。'}
+        format.js { render :index }
+      end
+    end
+  end
+
   private
     # ストロングパラメーター
     def comment_params
       params.require(:comment).permit(:blog_id, :content)
+    end
+
+    def set_comment
+      @comment = Comment.find(params[:id])
     end
 end

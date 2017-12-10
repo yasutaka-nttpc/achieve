1e59e6d8c0b4f1d1704b56703d361ac65d3f5fdd
yasutaka-nttpc <yasutaka@nttpc.co.jp>
2017-12-10 20:41:57 +0900

    action_mailer 設定修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,13 +78,13 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # action mailer setting
-  config.action_mailer.default_url_options = { host: 'https://glacial-ravine-47202.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'http://13.230.182.48/' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
     user_name:  ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
-    domain: "heroku.com",
+    domain: "13.230.182.48",
     address: "smtp.sendgrid.net",
     port: 587,
     authentication: :plain,

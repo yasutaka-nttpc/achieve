6e73fb9b10a27c2e1f8ecf9e94cb88dba92cabd8
yasutaka-nttpc <yasutaka@nttpc.co.jp>
2017-12-10 19:35:37 +0900

    バケット名設定修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,14 +12,10 @@ CarrierWave.configure do |config|
 
   case Rails.env
     when 'production'
-      config.fog_directory = '本番環境用のバケット名'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/本番環境用のバケット名'
+      config.fog_directory = 'achieve-production-yasutaka'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/achieve-production-yasutaka'
     when 'development'
-      config.fog_directory = '開発環境用のバケット名'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/開発環境用のバケット名'
-    when 'test'
-      config.fog_directory = 'テスト環境用のバケット名'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/テスト環境用のバケット名'
-      
+      config.fog_directory = 'achieve-development-yasutaka'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/achieve-development-yasutaka'
   end
 end
